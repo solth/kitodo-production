59a610880d1254dc2802c8d3558271ee8a334bc9
Arved Solth <solth@effective-webwork.de>
2026-01-08 19:57:10 +0100

    Refactor condition checking for some structure tree context menu options

--- Kitodo/src/test/java/org/kitodo/selenium/testframework/pages/MetadataEditorPage.java
+++ Kitodo/src/test/java/org/kitodo/selenium/testframework/pages/MetadataEditorPage.java
@@ -403,7 +403,7 @@ public class MetadataEditorPage extends Page<MetadataEditorPage> {
                         .findElement(By.id(SAVE_AND_EXIT_BUTTON_ID))::isDisplayed);
         WebElement contextMenu = Browser.getDriver().findElement(By.id("contextMenuLogicalTree"));
         List<WebElement> menuItems = contextMenu.findElements(By.className("ui-menuitem"));
-        assertEquals(5, menuItems.size(), "Wrong number of context menu items");
+        assertEquals(4, menuItems.size(), "Wrong number of context menu items");
         // click "add element" option
         menuItems.getFirst().click();
         // open "structure element type selection" menu
